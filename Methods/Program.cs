8894fa83e9025ff0afe38bc78cb17b1fc85d2523
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-01-22 13:40:25 -0500

    done

---
+++ Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("what is your favirate color?");
+            String color = Console.ReadLine();
+
+            Console.WriteLine("what is your dog name ?");
+            string dog = Console.ReadLine();
+
+            Console.WriteLine($"Hello Everyone, my name is {name}." +
+                $" My favirate color is {color}" +
+                $" and my dog name is {dog} ");
+        }
+    }
+}
